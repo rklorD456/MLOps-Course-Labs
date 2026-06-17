872f3be990f03f92a57f64128dfcf3829fe8299b
rklorD456 <wermasd192@gmail.com>
2026-06-17 13:06:08 +0300

    fix: update logging format to include logger name in logger_setup.py

--- app/logger_setup.py
+++ app/logger_setup.py
@@ -1,14 +1,17 @@
 """
 Logging configuration.
 """
+
 import logging
+
 
 def setup_logging():
     # TODO 1: Set up basic logging with level INFO using logging.basicConfig()
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-        
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
     # TODO 2: Create a named logger using logging.getLogger() and return it
     logger = logging.getLogger("churn_api")
     return logger
